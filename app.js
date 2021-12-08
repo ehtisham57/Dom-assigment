ca844ae7570c1991171805b4e386a92b390f9bbc
ehtisham57 <81214666+ehtisham57@users.noreply.github.com>
2021-12-08 21:50:26 +0500

    project

---
+++ app.js
@@ -0,0 +1,23 @@
+///Question 1
+
+// var a = document.getElementById("main-content")
+// console.log(a)
+// console.log(a.childNodes)
+
+// var b = document.getElementsByClassName("render")
+// console.log(b.innerHTML)
+
+// var b = document.getElementById("first-name")
+// b.value="Ehtisham"
+// console.log(b.value)
+
+
+
+//Question 2
+
+// var a = document.getElementById("form-content")
+// console.log(a.nodeType)
+
+// var a = document.getElementById("last-name")
+// console.log(a.nodeType)
+
